5b106fea9cddb31ad8b75b4d71f213ec3d7c9b39
christech1117 <christech1117@gmail.com>
2017-07-19 14:28:59 +0800

    完成首頁

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,33 +2,50 @@
   #Home
     .row
       .col-sm-3.col-md-3.col-lg-3.text-center
-        ul.list-group(v-for="(plan, index) in plans")
-          li.plan-card
-            router-link(:to="{ path: plan.url }")
-              img.rounded(v-if="!plan.active" :src="plan.image" @mouseover="plan.active=true" @mouseleave="plan.active=false")
-              img.rounded(v-if="plan.active" :src="plan.focus" @mouseover="plan.active=true" @mouseleave="plan.active=false")
-
+        ul.list-group
+          li
+            router-link(:to="{ path: '/plan/1' }")
+              img.rounded(src="/static/img/button/plan-1.png" onmouseover="this.src='/static/img/button/plan-1focus.png'" onmouseout="this.src='/static/img/button/plan-1.png'")
+          li
+            router-link(:to="{ path: '/plan/2' }")
+              img.rounded(src="/static/img/button/plan-2.png" onmouseover="this.src='/static/img/button/plan-2focus.png'" onmouseout="this.src='/static/img/button/plan-2.png'")
+          li
+            router-link(:to="{ path: '/plan/3' }")
+              img.rounded(src="/static/img/button/plan-3.png" onmouseover="this.src='/static/img/button/plan-3.png'" onmouseout="this.src='/static/img/button/plan-3.png'")
+          li
+            router-link(:to="{ path: '/plan/4' }")
+              img.rounded(src="/static/img/button/plan-4.png" onmouseover="this.src='/static/img/button/plan-4.png'" onmouseout="this.src='/static/img/button/plan-4.png'")
+          
       .col-sm-6.col-md-6.col-lg-6
         .row
           .col-sm-6.col-md-6.col-lg-6.work-2.text-right.work-menu
-            img.rounded(src="/static/img/button/work-2.png")
+            router-link(:to="{ path: '/work/1' }")
+              img.rounded(src="/static/img/button/work-1.png" onmouseover="this.src='/static/img/button/work-1focus.png'" onmouseout="this.src='/static/img/button/work-1.png'")
           .col-sm-6.col-md-6.col-lg-6.work-3.text-left.work-menu
-            img.rounded(src="/static/img/button/work-3.png")
+            router-link(:to="{ path: '/work/2' }")
+              img.rounded(src="/static/img/button/work-2.png" onmouseover="this.src='/static/img/button/work-2focus.png'" onmouseout="this.src='/static/img/button/work-2.png'")
           .col-sm-4.col-md-4.col-lg-4.work-4.text-center
-            img.rounded(src="/static/img/button/work-4.png")
+            router-link(:to="{ path: '/work/3' }")
+              img.rounded(src="/static/img/button/work-3.png" onmouseover="this.src='/static/img/button/work-3focus.png'" onmouseout="this.src='/static/img/button/work-3.png'")
           .col-sm-4.col-md-4.col-lg-4.work-6.text-center
-            img.rounded(src="/static/img/button/work-6.png")
+            router-link(:to="{ path: '/work/5' }")
+              img.rounded(src="/static/img/button/work-5.png" onmouseover="this.src='/static/img/button/work-5focus.png'" onmouseout="this.src='/static/img/button/work-5.png'")
           .col-sm-4.col-md-4.col-lg-4.work-5.text-center
-            img.rounded(src="/static/img/button/work-5.png")
-        //- .row
+            router-link(:to="{ path: '/work/4' }")
+              img.rounded(src="/static/img/button/work-4.png" onmouseover="this.src='/static/img/button/work-4focus.png'" onmouseout="this.src='/static/img/button/work-4.png'")
           
         
       .col-sm-3.col-md-3.col-lg-3.text-center
-        ul.list-group(v-for="scene in scenes")
-          li.plan-card
-            router-link(:to="{ path: scene.url }")
-              img.rounded(v-if="!scene.active" :src="scene.image" @mouseover="scene.active=true" @mouseleave="scene.active=false")
-              img.rounded(v-if="scene.active" :src="scene.focus" @mouseover="scene.active=true" @mouseleave="scene.active=false")
+        ul.list-group
+          li
+            router-link(:to="{ path: '/scene/1' }")
+              img.rounded(src="/static/img/button/scene-1.png" onmouseover="this.src='/static/img/button/scene-1focus.png'" onmouseout="this.src='/static/img/button/scene-1.png'")
+          li
+            router-link(:to="{ path: '/scene/2' }")
+              img.rounded(src="/static/img/button/scene-2.png" onmouseover="this.src='/static/img/button/scene-2focus.png'" onmouseout="this.src='/static/img/button/scene-2.png'")
+          li
+            router-link(:to="{ path: '/scene/3' }")
+              img.rounded(src="/static/img/button/scene-3focus.png" onmouseover="this.src='/static/img/button/scene-3focus.png'" onmouseout="this.src='/static/img/button/scene-3.png'")
           
     button.btn.btn-primary.btn-lg(type='button', data-toggle='modal', data-target='#flipFlop') 123
 
@@ -56,90 +73,90 @@ export default {
         {
           active: false,
           title: '計畫理念',
-          image: '/static/img/button/plan-2.png',
-          focus: '/static/img/button/plan-2focus.png',
+          image: '/static/img/button/plan-1.png',
+          focus: '/static/img/button/plan-1focus.png',
           url: '/plan/idea'
           
         },
         {
           active: false,
           title: '行政團隊',
-          image: '/static/img/button/plan-3.png',
-          focus: '/static/img/button/plan-3focus.png',
+          image: '/static/img/button/plan-2.png',
+          focus: '/static/img/button/plan-2focus.png',
           url: '/plan/work'
         },
         {
           active: false,
           title: '成果影片',
-          image: '/static/img/button/plan-4.png',
-          focus: '/static/img/button/plan-4.png',
+          image: '/static/img/button/plan-3.png',
+          focus: '/static/img/button/plan-3.png',
           url: '/plan/video'
         },
         {
           active: false,
           title: '成果影片2',
-          image: '/static/img/button/plan-5.png',
-          focus: '/static/img/button/plan-5.png',
+          image: '/static/img/button/plan-4.png',
+          focus: '/static/img/button/plan-4.png',
           url: '/plan/video2'
         }
       ],
-      works: [
-        {
-          active: false,
-          title: '雲想',
-          image: '/static/img/button/work-2.png',
-          focus: '/static/img/button/work-2focus.png',
-          url: '/work/1'
-        },
-        {
-          active: false,
-          title: '雲想',
-          image: '/static/img/button/work-3.png',
-          focus: '/static/img/button/work-3focus.png',
-          url: '/work/1'
-        },
-        {
-          active: false,
-          title: '雲想',
-          image: '/static/img/button/work-4.png',
-          focus: '/static/img/button/work-4focus.png',
-          url: '/work/1'
-        },
-        {
-          active: false,
-          title: '雲想',
-          image: '/static/img/button/work-5.png',
-          focus: '/static/img/button/work-5focus.png',
-          url: '/work/1'
-        },
-        {
-          active: false,
-          title: '雲想',
-          image: '/static/img/button/work-6.png',
-          focus: '/static/img/button/work-6focus.png',
-          url: '/work/1'
-        },
-      ],
+      // works: [
+      //   {
+      //     active: false,
+      //     title: '雲想',
+      //     image: '/static/img/button/work-2.png',
+      //     focus: '/static/img/button/work-2focus.png',
+      //     url: '/work/1'
+      //   },
+      //   {
+      //     active: false,
+      //     title: '大地之書',
+      //     image: '/static/img/button/work-3.png',
+      //     focus: '/static/img/button/work-3focus.png',
+      //     url: '/work/1'
+      //   },
+      //   {
+      //     active: false,
+      //     title: '光之舞',
+      //     image: '/static/img/button/work-4.png',
+      //     focus: '/static/img/button/work-4focus.png',
+      //     url: '/work/1'
+      //   },
+      //   {
+      //     active: false,
+      //     title: '復原．新生',
+      //     image: '/static/img/button/work-5.png',
+      //     focus: '/static/img/button/work-5focus.png',
+      //     url: '/work/1'
+      //   },
+      //   {
+      //     active: false,
+      //     title: '土地之聲',
+      //     image: '/static/img/button/work-6.png',
+      //     focus: '/static/img/button/work-6focus.png',
+      //     url: '/work/1'
+      //   },
+      // ],
       scenes: [
         {
           active: false,
           title: '計畫理念',
-          image: '/static/img/button/scene-2.png',
-          focus: '/static/img/button/scene-2focus.png',
+          image: '/static/img/button/scene-1.png',
+          focus: '/static/img/button/scene-1focus.png',
           url: '/scene/idea'
         },
         {
           active: false,
           title: '行政團隊',
-          image: '/static/img/button/scene-3.png',
+          image: '/static/img/button/scene-2.png',
           focus: '/static/img/button/scene-2focus.png',
           url: '/scene/work'
         },
         {
           active: false,
           title: '成果影片',
-          image: '/static/img/button/scene-4.png',
-          focus: '/static/img/button/scene-2focus.png',
+          image: '/static/img/button/scene-3.png',
+          focus: '/static/img/button/scene-3focus.png',
           url: '/scene/work'
         }
       ]
@@ -161,11 +178,11 @@ export default {
 
 *
   animation: fadeIn 1s ease both
-
 img
-  animation: fadeIn 2s ease both
   margin-bottom: 20px
   width: 70%
+  &:hover
+    // animation: fadeInUp 2s ease both
 
 .work-menu
   height: 320px
